0adb77acbb127749524ff0d2bc66dea3d28b6827
adrianyu91 <adrianyu91@gmail.com>
2022-08-31 01:42:02 -0400

    added ui into rps game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,18 +1,24 @@
 var playerScore=0, computerScore=0;
+//assigns the button with the value of its id in the html file which can determine which button is pressed during each click
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.id;
+        let computerChoice=getComputerChoice();
+        console.log(playerSelection);
+        displayPlayerChoice(button, computerChoice);
+        playRound(playerSelection,computerChoice);
+        replaceScore();
+        game();
+    });
+});
+
+//game loop that runs the games a certain number of times
 function game(){
-    for(let i=0; i<5;i++){
-        console.log("round "+ (i+1));
-        playRound(getPlayerChoice(),getComputerChoice());
-        console.log("player score: " + playerScore + " Computer score: " + computerScore);
-    }
-    if (playerScore>computerScore){
-        console.log("You have won!");
-    }
-    else if(computerScore>playerScore){
-        console.log("You have lost!");
-    }
-    else{
-        console.log("Tied");
+    if(playerScore===5 || computerScore===5){
+        document.querySelector('#🗿').disabled = true;
+        document.querySelector('#📄').disabled = true;
+        document.querySelector('#✂').disabled = true;
     }
 }
 
@@ -23,13 +29,13 @@ function getComputerChoice() {
     let computerChoice;
     switch(computerRandom){
         case 1:
-            computerChoice="Rock"
+            computerChoice="🗿"
             break;
         case 2:
-            computerChoice="Scissors"
+            computerChoice=" ✂"
             break;
         case 3:
-            computerChoice="Paper"
+            computerChoice="📄"
             break;
         default:
             console.log("error");
@@ -38,15 +44,83 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-//getting user's decision
-function getPlayerChoice(){
-    let playerChoice=prompt("Choose Rock Paper Scissors","");
-    playerChoice=capitalize(playerChoice);
-    console.log("Player choice: "+ playerChoice);
-    return playerChoice;
+//adds the players choice on the website
+const container = document.createElement('div');
+function displayPlayerChoice(button, computerChoice){
+    const roundChoice = document.querySelector('#game');
+    const container = document.createElement('div');
+    
+    //player
+    const yourChoice = document.createElement('span');
+    yourChoice.setAttribute('style', 'margin-right: 100px; font-size:80px; position: sticky;');
+
+    yourChoice.classList.add('yourChoice');
+    yourChoice.textContent=button.id;
+    container.appendChild(yourChoice)
+    roundChoice.appendChild(container);
+
+    
+    //computer
+    const computerChoice2 = document.createElement('span');
+    computerChoice2.setAttribute('style', 'font-size:80px; position: sticky;');
+
+    computerChoice2.classList.add('computerChoice');
+    computerChoice2.textContent=computerChoice;
+    container.appendChild(computerChoice2)
+    roundChoice.appendChild(container);
+}
+//Function of the game where it decides which player wins
+function playRound(playerSelection, computerSelection){
+    if(playerSelection===computerSelection){
+        console.log("draw");
+    }
+    else if(playerSelection==="🗿"){
+        if(computerSelection==="📄"){
+            console.log("Computer Wins");
+            computerScore++;
+        }
+        else{
+            console.log("Player Wins");
+            playerScore++;
+        }
+    }
+    else if(playerSelection==="📄"){
+        if(computerSelection==="✂"){
+            console.log("Computer Wins");
+            computerScore++;
+        }
+        else{
+            console.log("Player Wins");
+            playerScore++;
+        }
+    }
+    else if(playerSelection==="✂"){
+        if(computerSelection==="🗿"){
+            console.log("Computer Wins");
+            computerScore++;
+        }
+        else{
+            console.log("Player Wins");
+            playerScore++;
+        }
+    }
+}
+
+//Changes the scores to determine who wins the game
+function replaceScore(){
+    const scores = document.querySelector('#scores')
+    const playerCurrentScore = document.querySelector('#yourScore');
+    const computerCurrentScore = document.querySelector('#computerScore');
+    const tempScores =[];
+    tempScores[0]= playerCurrentScore.textContent;
+    tempScores[1]=computerCurrentScore.textContent;
+    //replace
+    playerCurrentScore.textContent=playerCurrentScore.textContent.replace(tempScores[0],playerScore);
+    computerCurrentScore.textContent=computerCurrentScore.textContent.replace(tempScores[1],computerScore);
 }
 
 //function to correct the captalization on user input
+//useless in v2 of the game
 function capitalize(word){
     let firstletter = word.slice(0,1);
     firstletter = firstletter.toUpperCase();
@@ -55,41 +129,3 @@ function capitalize(word){
     word = firstletter + restWord;
     return word;
 }
-
-function playRound(playerSelection, computerSelection){
-    if(playerSelection===computerSelection){
-        console.log("draw");
-    }
-    else if(playerSelection==="Rock"){
-        if(computerSelection==="Paper"){
-            console.log("Computer Wins");
-            computerScore++;
-        }
-        else{
-            console.log("Player Wins");
-            playerScore++;
-        }
-    }
-    else if(playerSelection==="Paper"){
-        if(computerSelection==="Scissors"){
-            console.log("Computer Wins");
-            computerScore++;
-        }
-        else{
-            console.log("Player Wins");
-            playerScore++;
-        }
-    }
-    else if(playerSelection==="Scissors"){
-        if(computerSelection==="Rock"){
-            console.log("Computer Wins");
-            computerScore++;
-        }
-        else{
-            console.log("Player Wins");
-            playerScore++;
-        }
-    }
-}
-
-game();
